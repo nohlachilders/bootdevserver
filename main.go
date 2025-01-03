499213dbabc51ddbc43412e2937e725573b72136
Nohl Childers <nohlachilders@github.com>
2025-01-02 18:04:50 -0800

    stuck on this one for now

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
+
 import (
-    //"fmt"
-    "net/http"
+	//"fmt"
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
 )
 
 func main() {
@@ -10,11 +14,33 @@ func main() {
         Handler: &servemux,
         Addr: ":8080",
     }
+    cfg := apiConfig{
+    }
 
-    servemux.Handle("/", http.FileServer(http.Dir(".")))
-    servemux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+    servemux.Handle("/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+    servemux.HandleFunc("/healthz", healthResponseHandler)
+    servemux.HandleFunc("/metrics", cfg.middlewareMetricsHandler)
 
-    server.ListenAndServe()
+    log.Fatal(server.ListenAndServe())
 }
 
+func healthResponseHandler(w http.ResponseWriter, req *http.Request) {
+    w.Header().Add("Content-Type","text/plain; charset=utf-8")
+    w.WriteHeader(200)
+    w.Write([]byte("OK"))
+}
 
+type apiConfig struct {
+    fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    cfg.fileserverHits.Add(4)
+    return next
+}
+
+func (cfg *apiConfig) middlewareMetricsHandler(w http.ResponseWriter, req *http.Request) {
+    hits := cfg.fileserverHits.Load()
+    str := fmt.Sprintf("Hits: %d", hits)
+    w.Write([]byte(str))
+}
